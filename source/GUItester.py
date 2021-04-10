e6c953cafaadebc7d90d6d316d215a5e53c5d26d
Braedon Kimball <48100828+bkimball98@users.noreply.github.com>
2021-04-10 14:17:04 -0500

    Connected driver code to flask, modified testing file to still work

--- source/GUItester.py
+++ source/GUItester.py
@@ -1,63 +1,52 @@
 #Written by Braedon Kimball,bmk228
 import unittest
-from cmdapp import BMI_Retirement
+from driver import bmi, retirement
 
 class TestBMICalculations(unittest.TestCase):
 
     def test_underweight(self):
-        br = BMI_Retirement()
-        br.bmi(120, 72)
-        self.assertEqual(br.cat, 'Underweight')
+        cat, out, wflag, hflag = bmi(120, 72)
+        self.assertEqual(cat, 'Underweight')
     def test_normal(self):
-        br = BMI_Retirement()
-        br.bmi(180, 72)
-        self.assertEqual(br.cat, 'Normal')
+        cat, out, wflag, hflag = bmi(180, 72)
+        self.assertEqual(cat, 'Normal')
     def test_obese(self):
-        br = BMI_Retirement()
-        br.bmi(300, 72)
-        self.assertEqual(br.cat, 'Obese')
+        cat, out, wflag, hflag = bmi(300, 72)
+        self.assertEqual(cat, 'Obese')
     def test_overweight(self):
-        br = BMI_Retirement()
-        br.bmi(215, 73)
-        self.assertEqual(br.cat, 'Overweight')
+        cat, out, wflag, hflag = bmi(215, 73)
+        self.assertEqual(cat, 'Overweight')
     def test_inputweight(self):
-        br = BMI_Retirement()
-        br.bmi(300, 68)
-        self.assertIs(br.wflag, True)
+        cat, out, wflag, hflag = bmi(300, 68)
+        self.assertIs(wflag, True)
     def test_inputheight(self):
-        br = BMI_Retirement()
-        br.bmi(300, 80)
-        self.assertIs(br.hflag, True)
+        cat, out, wflag, hflag = bmi(300, 80)
+        self.assertIs(hflag, True)
     
 
 class TestRetirement(unittest.TestCase):
 
     def test_alive(self): #Ensure that the age cap of 100 is met
-        br = BMI_Retirement()
-        br.retirement(25, 100000, 0.25, 2000000)
-        self.assertIs(br.flag, True)
+        m, flag, pflag, sflag, aflag = retirement(25, 100000, 0.25, 2000000)
+        self.assertIs(flag, True)
          
     def test_real_percentage(self):
-        br = BMI_Retirement()
-        br.retirement(25, 100000, 0.9, 2000000)
-        self.assertIs(br.pflag, True) #if pflag is true, then the percentage is between [0.0, 1.0]
+        m, flag, pflag, sflag, aflag = retirement(25, 100000, 0.9, 2000000)
+        self.assertIs(pflag, True) #if pflag is true, then the percentage is between [0.0, 1.0]
 
     def test_reasonable_income(self):
-        br = BMI_Retirement()
-        br.retirement(25, 10, 0.25, 2000000)
-        self.assertIs(br.sflag, True) #if sflag is true, then the salary is in the range (0, 500,000]
+        m, flag, pflag, sflag, aflag = retirement(25, 10, 0.25, 2000000)
+        self.assertIs(sflag, True) #if sflag is true, then the salary is in the range (0, 500,000]
 
     def test_reasonable_startage(self):
-        br = BMI_Retirement()
-        br.retirement(12, 500, 1, 5000)
-        self.assertIs(br.aflag, True)
+        m, flag, pflag, sflag, aflag = retirement(12, 500, 1, 5000)
+        self.assertIs(aflag, True)
 
     def test_reasonable_inputs(self):
-        br = BMI_Retirement()
-        br.retirement(32, 45000, 0.6, 1200000) #Ensure that all inputs are reasonable based on the defined acceptable ranges. 
-        self.assertIs(br.flag, True)
-        self.assertIs(br.pflag, True)
-        self.assertIs(br.sflag, True)
+        m, flag, pflag, sflag, aflag = retirement(32, 45000, 0.6, 1200000) #Ensure that all inputs are reasonable based on the defined acceptable ranges. 
+        self.assertIs(flag, True)
+        self.assertIs(pflag, True)
+        self.assertIs(sflag, True)
 
 if __name__ == "__main__":
     unittest.main()
